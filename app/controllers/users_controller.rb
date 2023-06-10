adff589cdf3ae768c4fed4a8cf3ad8ddb38886be
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-06-10 01:00:24 +0000

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,20 @@ class UsersController < ApplicationController
   protect_from_forgery
   
   def index
-   
     @user = User.all
   end
   
   def show
+    @user = User.find(params[:id])
+    @book = @user.book
+  end
+  
+  def edit
+    @user = User.find(params[:id])
   end
   
   
   def create
-   
     @user = User.index
     @user.user_id = current_user.id
     @user.save
